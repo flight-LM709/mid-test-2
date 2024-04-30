4bfd976573330140d1a5b6a0571b74e0bdeaafdb
flight-LM709 <150790738+flight-LM709@users.noreply.github.com>
2024-04-30 17:35:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function changeImage(element) {
+    element.setAttribute("src", "assets/img-header2.png")
+}
+
+function changeImageBack(element) {
+    element.setAttribute("src", "assets/img-header1.png")
+
+}
+
+var btnLike = document.querySelector('#green')
+var btnDislike = document.querySelector('#red')
+
+btnLike.onclick = likeColor
+btnDislike.onclick = dislikeColor
+
+function likeColor() {
+    if (btnDislike.classList.contains('red')) {
+        btnDislike.classList.remove('red')
+    }
+
+    this.classList.toggle('green')
+}
+
+function dislikeColor() {
+    if (btnLike.classList.contains('green')) {
+        btnLike.classList.remove('green')
+    }
+
+    this.classList.toggle('red')
+}
